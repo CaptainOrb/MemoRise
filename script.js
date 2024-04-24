107542f84244c570f28e1b6fb3acb4d30b58a4f9
CaptainOrb <135736141+CaptainOrb@users.noreply.github.com>
2024-04-24 23:55:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById("register-button1").addEventListener("click", redirect_startButton);
-document.getElementById("register-button2").addEventListener("click", redirect_startButton);
+document.getElementById("register-button1").addEventListener("click", redirect_register);
+document.getElementById("register-button2").addEventListener("click", redirect_register);
+document.getElementById("start-button").addEventListener("click", redirect_register);
 
 function redirect_register() {
     window.location = "codeLoginRegister/register.html"
@@ -9,10 +10,4 @@ document.getElementById("login-button").addEventListener("click", redirect_login
 
 function redirect_login(){
     window.location = "codeLoginRegister/login.html"
-}
-
-document.getElementById("start-button").addEventListener("click", redirect_startButton);
-
-function redirect_startButton(){
-    window.location = "codeLoginRegister/register.html"
 }
