2155d135d2d4d10a11ab16f3f15b4e69aa99eeae
CaptainOrb <135736141+CaptainOrb@users.noreply.github.com>
2024-04-24 23:21:17 +0700

    fix index.html

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.getElementById("register-button").addEventListener("click", redirect_register);
+document.getElementsByClassName("register-button").addEventListener("click", redirect_register);
 
 function redirect_register(){
-    window.location = "../codeLoginRegister/register.html"
+    window.location = "codeLoginRegister/register.html"
 }
 
 document.getElementById("login-button").addEventListener("click", redirect_login);
 
 function redirect_login(){
-    window.location = "../codeLoginRegister/login.html"
+    window.location = "codeLoginRegister/login.html"
 }
 
 document.getElementById("start-button").addEventListener("click", redirect_startButton);
 
 function redirect_startButton(){
-    window.location = "../codeLoginRegister/register.html"
+    window.location = "codeLoginRegister/register.html"
 }
